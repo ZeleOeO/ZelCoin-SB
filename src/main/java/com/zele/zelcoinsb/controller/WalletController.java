1ca1e78a2d93822ab3a6c4e376de3ead365a8796
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-27 14:42:00 +0100

    feat: create wallet controller add endpoint to retrieve wallet create wallet

---
+++ src/main/java/com/zele/zelcoinsb/controller/WalletController.java
@@ -0,0 +1,31 @@
+package com.zele.zelcoinsb.controller;
+
+import com.zele.zelcoinsb.models.dtos.wallet.WalletViewDTO;
+import com.zele.zelcoinsb.service.WalletService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/wallets")
+@AllArgsConstructor
+public class WalletController {
+    private WalletService walletService;
+
+    @GetMapping()
+    public List<WalletViewDTO> getAllWallets() {
+        return walletService.getAllWallets();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WalletViewDTO> getWalletById(@PathVariable Long id) {
+        return walletService.getWalletById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<WalletViewDTO> createWallet() {
+        return walletService.createWalletController();
+    }
+}
