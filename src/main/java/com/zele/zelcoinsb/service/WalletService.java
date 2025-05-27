11e3675184f121446bdd9a81cee7cb39aaef4198
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-27 13:50:37 +0100

    feat: create methods in wallet service to retrieve wallet create wallet transact wallet

--- src/main/java/com/zele/zelcoinsb/service/WalletService.java
+++ src/main/java/com/zele/zelcoinsb/service/WalletService.java
@@ -1,55 +1,80 @@
 package com.zele.zelcoinsb.service;
 
+import com.zele.zelcoinsb.exceptions.wallet.WalletInsufficientFundsException;
+import com.zele.zelcoinsb.exceptions.wallet.WalletNotFoundException;
+import com.zele.zelcoinsb.exceptions.wallet.WalletSignatureErrorException;
+import com.zele.zelcoinsb.mapper.WalletMapper;
+import com.zele.zelcoinsb.models.dtos.wallet.WalletViewDTO;
 import com.zele.zelcoinsb.models.entities.Transaction;
 import com.zele.zelcoinsb.models.entities.Wallet;
+import com.zele.zelcoinsb.repository.WalletRepository;
 import com.zele.zelcoinsb.tools.CustomKeyPairGenerator;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
+@AllArgsConstructor
 public class WalletService {
     private final BlockChainService blockChainService;
     private final TransactionService transactionService;
+    private final WalletRepository walletRepository;
+    private final WalletMapper walletMapper;
 
-    public WalletService(@Lazy BlockChainService blockChainService, TransactionService transactionService) {
-        this.blockChainService = blockChainService;
-        this.transactionService = transactionService;
+    public List<WalletViewDTO> getAllWallets() {
+        return walletRepository.findAll()
+                .stream()
+                .map(walletMapper::toWalletViewDTO)
+                .toList();
     }
 
-    public Wallet createWallet() {
-        Wallet wallet = new Wallet();
-        var keypair = CustomKeyPairGenerator.generateKeyPair();
-        wallet.setPrivateKey(keypair.getPrivate());
-        wallet.setPublicKey(keypair.getPublic());
-        wallet.setBalance(0.0);
-        return wallet;
+    public ResponseEntity<WalletViewDTO> getWalletById(Long wallet_id) {
+        var wallet = walletRepository.findById(wallet_id).orElse(null);
+        checkWalletInDB(wallet);
+        return ResponseEntity.status(HttpStatus.OK).body(walletMapper.toWalletViewDTO(wallet));
     }
 
-    public void creditWallet(PublicKey publicKey, Double amount) {
-        //TODO implement find wallet by public key to update balance
-//        wallet.setBalance(balance);
+    public ResponseEntity<WalletViewDTO> getWalletByWalletAddress(PublicKey publicKey) {
+        var wallet = walletRepository.findByPublicKey(publicKey).orElse(null);
+        checkWalletInDB(wallet);
+        return ResponseEntity.status(HttpStatus.OK).body(walletMapper.toWalletViewDTO(wallet));
     }
-     public void debitWallet(PublicKey wallet, Double amount) {
-         //TODO implement find wallet by public key to update balance
-//        wallet.setBalance(balance);
+
+    public ResponseEntity<WalletViewDTO> createWalletController() {
+        Wallet wallet = createWallet();
+        walletRepository.save(wallet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(walletMapper.toWalletViewDTO(wallet));
+    }
+
+     public ResponseEntity<WalletViewDTO> transact(PublicKey sender, PublicKey receiver, Double amount) {
+        var senderWallet = walletRepository.findByPublicKey(sender).orElse(null);
+        var receiverWallet = walletRepository.findByPublicKey(receiver).orElse(null);
+        checkWalletInDB(senderWallet);
+        checkWalletInDB(receiverWallet);
+        sendMoney(amount, senderWallet, receiver);
+        senderWallet.setBalance(senderWallet.getBalance() - amount);
+        receiverWallet.setBalance(receiverWallet.getBalance() + amount);
+        walletRepository.save(senderWallet);
+        walletRepository.save(receiverWallet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(walletMapper.toWalletViewDTO(walletRepository.save(senderWallet)));
      }
 
-     public void transact(PublicKey sender, PublicKey receiver, Double amount) {
-        debitWallet(sender, amount);
-        creditWallet(sender, amount);
-     }
+    // Helper Methods
+    private void checkWalletInDB(Wallet wallet) {
+        if (wallet==null) throw new WalletNotFoundException("Wallet not found in DB.");
+    }
 
     public void sendMoney(Double amount, Wallet senderWallet, PublicKey receiverPublicKey) {
-        Logger logger = Logger.getLogger(WalletService.class.getName());
         if (senderWallet.getBalance() < amount) {
-            logger.log(Level.SEVERE, "Insufficient funds");
-            return;
+            throw new WalletInsufficientFundsException("Insufficient Funds");
         }
         Transaction transaction = transactionService.createTransaction(amount, senderWallet.getPublicKey(), receiverPublicKey);
         Signature sign;
@@ -60,8 +85,17 @@ public class WalletService {
             sign.update(transaction.toString().getBytes(StandardCharsets.UTF_8));
             signature = sign.sign();
         } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
-            logger.log(Level.SEVERE, e.getMessage());
+            throw new WalletSignatureErrorException(e.getMessage());
         }
         blockChainService.addBlock(transaction, senderWallet.getPublicKey(), signature);
+    }
+
+    public Wallet createWallet() {
+        Wallet wallet = new Wallet();
+        var keypair = CustomKeyPairGenerator.generateKeyPair();
+        wallet.setPrivateKey(keypair.getPrivate());
+        wallet.setPublicKey(keypair.getPublic());
+        wallet.setBalance(10.0);
+        return wallet;
     }
 }
