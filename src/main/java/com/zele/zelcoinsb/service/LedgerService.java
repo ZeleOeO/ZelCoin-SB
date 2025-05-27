0e595a08db255969d63498acc44768323af1f273
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-27 11:09:21 +0100

    feat: change ledger to store transactions

--- src/main/java/com/zele/zelcoinsb/service/LedgerService.java
+++ src/main/java/com/zele/zelcoinsb/service/LedgerService.java
@@ -14,33 +14,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 @AllArgsConstructor
 public class LedgerService {
-    private final Map<PublicKey, Double> balances = new ConcurrentHashMap<PublicKey, Double>() {
-    };
-
-    public void credit(PublicKey publicKey, Double amount) {
-        double newBalance = getBalance(publicKey) + amount;
-        balances.replace(publicKey, newBalance);
-    }
-
-    private void debit(PublicKey publicKey, Double amount) {
-        double newBalance = getBalance(publicKey) - amount;
-        balances.replace(publicKey, newBalance);
-    }
-
-    public void addPublicKey(PublicKey publicKey, Double amount) {
-        balances.put(publicKey, amount);
-    }
-
-    public void transact(PublicKey publicKeySender, PublicKey publicKeyReceiver, Double amount) {
-        credit(publicKeyReceiver, amount);
-        debit(publicKeySender, amount);
-    }
-
-    public double getBalance(PublicKey publicKey) {
-        return balances.get(publicKey);
-    }
-
-    public Map<PublicKey, Double> getBalances() {
-        return balances;
+    private final List<Transaction> transactions;
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
     }
 }
