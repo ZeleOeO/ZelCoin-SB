29781b91ee2f1aab95f7d891c85164660fd4c5df
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-27 13:48:46 +0100

    feat: create wallet view dto and mapper to map it to wallet class

---
+++ src/main/java/com/zele/zelcoinsb/mapper/WalletMapper.java
@@ -0,0 +1,19 @@
+package com.zele.zelcoinsb.mapper;
+
+import com.zele.zelcoinsb.models.dtos.wallet.WalletViewDTO;
+import com.zele.zelcoinsb.models.entities.Wallet;
+import com.zele.zelcoinsb.service.WalletService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class WalletMapper {
+    public WalletViewDTO toWalletViewDTO(Wallet wallet) {
+        WalletViewDTO walletViewDTO = new WalletViewDTO();
+        walletViewDTO.setId(wallet.getId());
+        walletViewDTO.setWalletAddress(wallet.getPublicKey());
+        walletViewDTO.setAccountBalance(wallet.getBalance());
+        return walletViewDTO;
+    }
+}
---
+++ src/main/java/com/zele/zelcoinsb/models/dtos/wallet/WalletViewDTO.java
@@ -0,0 +1,21 @@
+package com.zele.zelcoinsb.models.dtos.wallet;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.security.PublicKey;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WalletViewDTO {
+    private Long id;
+    private PublicKey walletAddress;
+    private Double accountBalance;
+}
